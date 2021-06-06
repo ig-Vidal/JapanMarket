087e562df5d1144b981219fadea84c701b03211c
eros-v <82416029+eros-v@users.noreply.github.com>
2021-06-06 17:33:47 -0400

    Add files via upload

---
+++ assets/js/validador.js
@@ -0,0 +1,17 @@
+// objeto.metodo(json)
+
+$('#formulario').validate({ 
+    "rules": {
+        "txtEmail": {
+            required: true,
+            email: true,
+        }
+    }, // --> Fin de reglas
+    messages: {
+        "txtEmail": {
+            required: 'El email es un campo requerido',
+            email: 'El email no cumple el formato de un correo'
+
+        }
+    }
+});
